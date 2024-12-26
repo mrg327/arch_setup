b8d26802a54e226a6012f0cba2f57493b1b084fc
mrg327 <mrg327>
2024-12-26 20:14:51 +0000

    Add neovim 10.2 to the repo

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Download and install the cirrus audio drivers for Macbook
+cd ~/Downloads
+git clone https://github.com/davidjo/snd_hda_macbookpro.git
+cd snd_hda_macbookpro
+./install.cirrus.driver.sh
+
+pacman -S neovim
