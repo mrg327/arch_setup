68604658d1631f9919866d18c8e683e151e32e93
mrg327 <mrg327>
2024-12-26 20:32:15 +0000

    Add a TODO section to the file of future fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
+# NOTE: This will get audio to work on Mac
 # Download and install the cirrus audio drivers for Macbook
 cd ~/Downloads
-git clone https://github.com/davidjo/snd_hda_macbookpro.git
-cd snd_hda_macbookpro
+git clone https://github.com/davidjo/snd_hda_macbookpro.git ~/Downloads/
+cd ~/Downloads/snd_hda_macbookpro
 ./install.cirrus.driver.sh
+cd ~
+
+pacman -S gcc linux-headers make patch wget
 
 pacman -S neovim
+
+# TODO: Fix env issues with arch macbook
+# [ ] Get bluetooth working
+# [ ] Make it so suspending the system does not require power off
