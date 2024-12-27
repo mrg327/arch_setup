356eee84ac0ec92d0ea98d2286fcbf34e5153414
mrg327 <matthewghere327@gmail.com>
2024-12-27 03:57:21 +0000

    Comment the total install script

--- install.sh
+++ install.sh
@@ -1,29 +1,76 @@
-# NOTE: This will get audio to work on Mac
-# Download and install the cirrus audio drivers for Macbook
+#!/usr/bin/env bash
 
+##
+# MacBook Arch Workarounds Script
+#
+
+# Make sure we run as root (or comment out if you prefer manual sudo).
+if [[ $EUID -ne 0 ]]; then
+   echo "This script needs to be run as root (or with sudo)."
+   exit 1
+fi
+
+##################################
+# 1) MacBook audio workaround
+##################################
 read -p "Install macbook audio workaround (y/n)?: " audio
-if [[$audio == "y"]]; then
-cd ~/Downloads
-git clone https://github.com/davidjo/snd_hda_macbookpro.git ~/Downloads/snd_hda_macbookpro
-cd ~/Downloads/snd_hda_macbookpro
-./install.cirrus.driver.sh
-cd ~
+if [[ "$audio" == "y" ]]; then
+  # Install dependencies if needed
+  # pacman -S --needed git base-devel
+
+  cd ~/Downloads || exit
+  git clone https://github.com/davidjo/snd_hda_macbookpro.git
+  cd snd_hda_macbookpro || exit
+
+  # This script from the repo should compile & install the driver
+  # Make sure it has +x permissions
+  chmod +x install.cirrus.driver.sh
+  ./install.cirrus.driver.sh
+
+  cd ~ || exit
+  echo "MacBook Cirrus audio driver installed."
 fi
 
+##################################
+# 2) Install Neovim
+##################################
 read -p "Install Neovim 10.2 (y/n)?: " neovim
-if [[$neovim == "y"]]; then
-pacman -S neovim
+if [[ "$neovim" == "y" ]]; then
+  # If your Neovim binary is in the current directory:
+  # adjust $PWD or path to wherever your local nvim is
+  if [[ -f "$PWD/nvim" ]]; then
+    cp "$PWD/nvim" /usr/bin/nvim
+    chmod +x /usr/bin/nvim
+  else
+    echo "Could not find $PWD/nvim. Adjust this path to your local binary."
+  fi
+
+  # Kickstart config (example)
+  git clone git@github.com:mrg327/kickstart.nvim.git ~/.config/nvim
+  echo "Neovim and kickstart.nvim installed."
 fi
 
-# NOTE: TO get bluetooth working
-read -p "Install macbook bluetooth workaround (y/n)?: " bluetooth 
-if [[$bluetooth == "y"]]; then
-git clone https://github.com/leifliddy/macbook12-bluetooth-driver.git ~/Downloads/macbook12-bluetooth-driver/
-cd ~/Downloads/macbook12-bluetooth-driver/ 
-pacman -S bluez blues-utils bluez-hid2hci
+##################################
+# 3) Bluetooth workaround
+##################################
+read -p "Install macbook bluetooth workaround (y/n)?: " bluetooth
+if [[ "$bluetooth" == "y" ]]; then
+  # pacman -S --needed git bluez bluez-utils bluez-hid2hci
+  cd ~/Downloads || exit
+  git clone https://github.com/leifliddy/macbook12-bluetooth-driver.git
+  cd macbook12-bluetooth-driver || exit
+
+  # In some cases, you might need to compile or add a DKMS module
+  # For example, if there's a PKGBUILD, you'd do:
+  # makepkg -si
+  # Or if there's a script, run it similarly:
+  # chmod +x some-script.sh
+  # ./some-script.sh
+
+  echo "Bluetooth driver workaround installed."
+  echo "Remember to enable and start bluetooth service:"
+  echo "  systemctl enable bluetooth"
+  echo "  systemctl start bluetooth"
 fi
 
-# TODO: Fix env issues with arch macbook
-# [x] Get audio working
-# [x] Get bluetooth working
-# [ ] Make it so suspending the system does not require power off
+echo "All done. Remember to address any additional steps as indicated."
